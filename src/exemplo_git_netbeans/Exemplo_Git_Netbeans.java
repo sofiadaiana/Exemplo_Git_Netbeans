0713267cd31b9daf3dc118e4491f44f2fd791489
Sofia <>
2017-05-16 18:45:41 -0300

    1º committ no meu netbenas

---
+++ src/exemplo_git_netbeans/Exemplo_Git_Netbeans.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo_git_netbeans;
+
+/**
+ *
+ * @author Sofia
+ */
+public class Exemplo_Git_Netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Exemplo de projeto Git e Netbeans");
+    }
+    
+}
